8252cc8066deeed5d7ed75c46a218315c5b79377
Mahdi K. <106227973+m2k7m@users.noreply.github.com>
2024-07-18 16:59:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import zipfile,os,shutil,gdown,time
 
 def CheckUSB():
-    usb = os.popen("wmic logicaldisk where drivetype=2 get deviceid").read()
+    usb = os.popen("wmic logicaldisk where drivetype=2 get filesystem,volumename,deviceid").read()
     if usb.find("DeviceID") != -1:
-        N1 = usb.split(":")[0].split("\n")[2]
-        return N1 + ":"
+        usb_letter = usb.split("\n")[2].split(" ")[0]
+        usb_system = usb.split("\n")[2].split(" ")[8]
+        usb_name = usb.split("\n")[2].split(" ")[15]
+        return usb_letter,usb_system,usb_name
     else:
         os.system('cls' if os.name=='nt' else 'clear')
         print("\033[0;31mThere's No USB, Waiting for a USB ...")
         while True:
-            usb = os.popen("wmic logicaldisk where drivetype=2 get deviceid").read()
+            usb = os.popen("wmic logicaldisk where drivetype=2 get filesystem,volumename,deviceid").read()
             if usb.find("DeviceID") != -1:
-                N1 = usb.split(":")[0].split("\n")[2]
-                return N1 + ":"
+                usb_letter = usb.split("\n")[2].split(" ")[0]
+                usb_system = usb.split("\n")[2].split(" ")[8]
+                usb_name = usb.split("\n")[2].split(" ")[15]
+                return usb_letter,usb_system,usb_name
             time.sleep(5)
 
 def fromZiptoUSB(zipname:str):
@@ -21,13 +25,13 @@ def fromZiptoUSB(zipname:str):
         with zipfile.ZipFile(zipname) as newzip:
             print(f"\033[32mPrepareing {zipname} ...\033[0m")
             if newzip.filelist[0].filename.startswith("PS4/"):
-                newzip.extractall(check)
+                newzip.extractall(check[0])
                 print("\033[32mYour PS4 Save Is Ready, Enjoy <3")
             else:
-                newzip.extractall(check)
-                shutil.copytree(check + newzip.filelist[0].filename,check,dirs_exist_ok=True)
-                shutil.rmtree(check + newzip.filelist[0].filename)
-                print(f"\033[32mI Moved Your Save To {check}, Enjoy <3")
+                newzip.extractall(check[0])
+                shutil.copytree(check[0] + newzip.filelist[0].filename,check[0],dirs_exist_ok=True)
+                shutil.rmtree(check[0] + newzip.filelist[0].filename)
+                print(f"\033[32mI Moved Your Save To {check[2]}, Enjoy <3")
 
 def fromGDrivetoUSB(url:str):
     if url.startswith("https://drive.google.com/file/d/"):
@@ -52,7 +56,7 @@ def main():
     check = CheckUSB()
     if check:
         os.system('cls' if os.name=='nt' else 'clear')
-        print(f"\033[32mUSB Found {check} \033[0m")
+        print(f"\033[32mUSB Found {check[2]} \033[0m")
         Value = input("Input a Value: ")
         if Value.startswith("https://drive.google.com") or Value.startswith("https://drive.usercontent.google.com"): 
             fromGDrivetoUSB(Value)
