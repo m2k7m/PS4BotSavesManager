725d3d787361db0662376a12a53931b634815a20
m2k7m <106227973+m2k7m@users.noreply.github.com>
2024-07-18 15:26:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import zipfile,os,shutil,gdown
 
 def CheckUSB():
     Usb = os.popen("wmic logicaldisk where drivetype=2 get deviceid").read()
-
     if Usb.find("DeviceID") != -1:
         N1 = Usb.split(":")[0].split("\n")[2]
-        print(f"USB Is Plugged, Found {N1}:")
         return N1 + ":"
     else:
         print("\033[0;31mThere's No USB")
@@ -18,12 +16,12 @@ def fromZiptoUSB(zipname:str):
         with zipfile.ZipFile(zipname) as newzip:
             if newzip.filelist[0].filename.startswith("PS4/"):
                 newzip.extractall(check)
-                print("Your PS4 Save Is Ready, Enjoy <3")
+                print("\033[32mYour PS4 Save Is Ready, Enjoy <3")
             else:
                 newzip.extractall(check)
-                shutil.move(check + newzip.filelist[1].filename,check)
+                shutil.copytree(check + newzip.filelist[0].filename,check,dirs_exist_ok=True)
                 shutil.rmtree(check + newzip.filelist[0].filename)
-                print("I Moved Your Save To The Right Place, Enjoy <3")
+                print(f"\033[32mI Moved Your Save To {check}, Enjoy <3")
 
 def fromGDrivetoUSB(url:str):
     if url.startswith("https://drive.google.com/file/d/"):
@@ -37,12 +35,22 @@ def fromGDrivetoUSB(url:str):
     dzip = gdown.download("https://docs.google.com/uc?export=download&id=" + file_id)
     if dzip.endswith(".zip"):
         fromZiptoUSB(dzip)
+    else:
+        print("\033[0;31mThe Link Is Not For a Save.")
         os.remove(dzip)
-        
-start = input("Input a Value: ")
-if start.startswith("https://drive.google.com"):
-    print("Waiting For Download ...")
-    fromGDrivetoUSB(start)
-if start.endswith(".zip"):
-    print(f"Prepareing {start} ...")
-    fromZiptoUSB(start)
+        input("")
+        exit()
+
+def main():
+    check = CheckUSB()
+    print(f"\033[32mUSB Found {check} \033[0m")
+    start = input("Input a Value: ")
+    if check:
+        if start.startswith("https://drive.google.com"):  
+            print("\033[32mWaiting For Download ...\033[0m")
+            fromGDrivetoUSB(start)
+        if start.endswith(".zip"):
+            print(f"\033[32mPrepareing {start} ...\033[0m")
+            fromZiptoUSB(start)
+
+main()
